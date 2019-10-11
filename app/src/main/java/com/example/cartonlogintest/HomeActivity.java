7bd2b211cb58f9bfdc821ec5ac9be8ce65d8e0b8
Adham Kassem <adham.kassem1234@gmail.com>
2019-10-11 17:43:25 -0500

    updated more

--- app/src/main/java/com/example/cartonlogintest/HomeActivity.java
+++ app/src/main/java/com/example/cartonlogintest/HomeActivity.java
@@ -2,13 +2,31 @@ package com.example.cartonlogintest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+    Button logout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        logout = findViewById(R.id.btnLogout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent inToMain = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(inToMain);
+            }
+        });
     }
 }
